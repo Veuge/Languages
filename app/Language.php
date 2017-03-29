94dedb41a5f34fb6839b80c0f0b5bfa72b9072b2
Veuge <nicky.0592@gmail.com>
2017-03-29 10:33:23 -0400

    Eloquent for Language and Stats

--- app/Language.php
+++ app/Language.php
@@ -15,4 +15,8 @@ class Language extends Model
     public function projects(){
         return $this->belongsToMany('App\Project', 'language_project', 'lang_id', 'project_id');
     }
+
+    public function stats(){
+        return $this->belongsToMany('App\StatType', 'github_stats', 'lang_id', 'type_id')-> withPivot('quantity');
+    }
 }
