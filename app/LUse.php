c41e0e5e0ea2cdaf70ff905c698d98f4bce919b9
Veuge <nicky.0592@gmail.com>
2017-03-29 10:23:06 -0400

    Eloquent for Language and Use

--- app/LUse.php
+++ app/LUse.php
@@ -4,6 +4,11 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class LUse extends Model {
+class LUse extends Model
+{
     public $timestamps = false;
+
+    public function languages(){
+        return $this->belongsToMany('App\Language', 'language_use', 'use_id', 'lang_id');
+    }
 }
