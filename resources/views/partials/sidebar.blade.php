66a88d1c63b0e27f690441df886b04b2778903d8
Veuge <nicky.0592@gmail.com>
2017-03-29 13:08:35 -0400

    Template added

---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,31 @@
+<!-- Left side column. contains the sidebar -->
+<aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+        <!-- sidebar menu: : style can be found in sidebar.less -->
+        <ul class="sidebar-menu">
+            <li class="header">MENU PRINCIPAL</li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-language"></i> <span>Lenguajes de programación</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-cog"></i> <span>Areas de uso</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-mortar-board"></i> <span>Proyectos</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-area-chart"></i> <span>Estadísticas</span>
+                </a>
+            </li>
+        </ul>
+    </section>
+    <!-- /.sidebar -->
+</aside>
